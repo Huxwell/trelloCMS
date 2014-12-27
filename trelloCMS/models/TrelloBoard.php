47c5792342d7765f32b72edf9d089a817ae45b5c
drapej <filip.drapejkowski@gmail.com>
2014-12-27 01:41:09 +0100

    Closed /archived/ lists fix.

--- trelloCMS/models/TrelloBoard.php
+++ trelloCMS/models/TrelloBoard.php
@@ -11,11 +11,12 @@ class TrelloBoard {
         $json_object = json_decode($json);
         $this->name = $json_object->name;
         foreach ($json_object->lists as $list) {
+            if($list->closed == false)
             $this->lists[$list->id] = new TrelloList($list->id, $list->name);
         }
         foreach ($json_object->cards as $card) {
             if (array_key_exists($card->idList, $this->lists))
-                $this->lists[$card->idList]->addCard($card);
+                if($this->lists[$card->idList])$this->lists[$card->idList]->addCard($card);
         }
     }
 
