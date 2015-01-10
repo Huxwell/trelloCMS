ee6c01d7c466cb0ef9fc9abd0df435801823714f
drapej <filip.drapejkowski@gmail.com>
2015-01-10 20:21:30 +0100

    v2.1 - templates. TODO: determine why some functions are called too often

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
 (function() {
-    var app = angular.module('trelloCMS',[]);
+    var app = angular.module('trelloCMS', []);
 
-    app.controller('TrelloController', ['$http', '$location', '$scope', function($http,$location,$scope) {
+    app.controller('TrelloController', ['$http', '$location', '$scope', function($http, $location, $scope) {
             var trello = this;
-            this.currListId = $location.path();
+
             trello.board = [];
-            
             $http.get('trello.json').success(function(data) {
                 trello.board = data;
+                // For reasons i dont understand it gets called too often
+                console.log(trello.board);
             });
-            
-            $scope.isActive = function(viewLocation) {
-              return viewLocation === $location.path();  
+            $scope.currList = function(lists) {
+                console.log(lists);
+                trello.currListId = $location.path().replace("/", "");
+                lists.some(function(el) {
+                    if (trello.currListId === el.id) {
+                        trello.currListName = el.name;
+                        // For reasons i dont understand it gets called too often
+                        return true;
+                    }
+                });
+                templateProposition =  'templates/' + trello.currListName + '.html';
+                trello.currTemplate = pageExists(templateProposition) ? templateProposition : "templates/default.html";
             };
-            
+            function pageExists(url)
+            {
+                var http = new XMLHttpRequest();
+                http.open('HEAD', url, false);
+                http.send();
+                return http.status != 404;
+            }
         }]);
     app.directive("trelloMenu", function() {
-      return {
-        restrict:"E",
-        templateUrl: "trello-menu.html"
-      };
+        return {
+            restrict: "E",
+            templateUrl: "trello-menu.html"
+        };
     });
-    app.directive("trelloList", function() {
-      return {
-        restrict:"E",
-        templateUrl: "trello-list.html"
-      };
-    });
-
 })();
